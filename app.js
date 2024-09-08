f71e704762da4bf56e1b0ab1a1ee5445eac8a203
Alan Navarrete <alan.n@craftedwebsolutionsllc.com>
2024-09-08 14:47:34 -0700

    check point

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.getElementById('hamburger').addEventListener('click', function() {
+document.getElementById('hamburger').addEventListener('click', function () {
   const navLinks = document.getElementById('nav-links');
   navLinks.classList.toggle('active');
 });
